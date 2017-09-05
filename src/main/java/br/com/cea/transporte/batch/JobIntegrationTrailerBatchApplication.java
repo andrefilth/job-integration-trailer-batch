c7f5a6e0692131811ee143f0212bfdbdb9f42710
André <andrelgfranco@gmail.com>
2017-09-05 13:00:54 -0300

    Primeiro commit

---
+++ src/main/java/br/com/cea/transporte/batch/JobIntegrationTrailerBatchApplication.java
@@ -0,0 +1,12 @@
+package br.com.cea.transporte.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobIntegrationTrailerBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobIntegrationTrailerBatchApplication.class, args);
+	}
+}
